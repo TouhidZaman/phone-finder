668e36e6b125c4b3cb711653f24e9e3b65ba75d4
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-03-01 05:02:45 +0600

    show phone details UI and handler added

--- js/phones-api.js
+++ js/phones-api.js
@@ -34,7 +34,7 @@ const loadPhones = data => {
                         <p class="card-text">Brand: ${phone.brand}</p>
                     </div>
                     <div class="py-2 d-flex justify-content-center">
-                        <button class="btn btn-secondary rounded-pill px-4">Show Details</button>
+                        <button onclick="loadPhoneBySlug('${phone.slug}')" class="btn btn-secondary rounded-pill px-4">Show Details</button>
                     </div>
                 </div>
             `;
@@ -43,4 +43,32 @@ const loadPhones = data => {
             console.log(phone)
         });
     }
+}
+
+const loadPhoneBySlug = slug => {
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showPhoneDetails(data.data))
+}
+
+const showPhoneDetails = phone => {
+    const showPhoneDetailsField = document.getElementById('showPhoneDetailsField');
+    showPhoneDetailsField.textContent = '';
+    const phoneCard = document.createElement('div');
+    phoneCard.innerHTML = `
+        <div class="card p-4 w-75 mx-auto">
+            <div class="py-2">
+                <img src="${phone.image}" class="img-fluid" alt="phone-img">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Name: ${phone.name}</h5>
+                <p class="card-text">Brand: ${phone.brand}</p>
+                <p class="card-text">Sensors: ${phone.mainFeatures.sensors?.forEach(sensor => {
+                    console.log(sensor);
+                })}</p>
+            </div>
+        </div>
+    `;
+    showPhoneDetailsField.appendChild(phoneCard);
 }
