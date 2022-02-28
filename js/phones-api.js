75cb6494260951601ae612a55ab06fe6118dd313
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-03-01 04:10:18 +0600

    UI adjusted and phones not found handler added

--- js/phones-api.js
+++ js/phones-api.js
@@ -11,26 +11,36 @@ const getPhonesByBrand = () => {
 
 //Loading phones data from api to UI
 const loadPhones = data => {
-    const phones =  data.data;
+    const notFoundField = document.getElementById('not-found-field');
     const phonesContainerField = document.getElementById('phones-container');
-    phones.forEach(phone => {
-        const colCard = document.createElement('div');
-        colCard.innerHTML = `
-            <div class="card py-4 b-none shadow h-100">
-                <div class="py-2 d-flex justify-content-center">
-                    <img src="${phone.image}" class="img-fluid" alt="phone-img">
+    phonesContainerField.textContent = '';
+    
+    if(data.status === false) {
+        notFoundField.classList.remove('d-none');
+        notFoundField.classList.add('d-block');
+    }
+    else {
+        notFoundField.classList.add('d-none');
+        const phones =  data.data;
+        phones.forEach(phone => {
+            const colCard = document.createElement('div');
+            colCard.innerHTML = `
+                <div class="card py-4 b-none shadow h-100">
+                    <div class="py-2 d-flex justify-content-center">
+                        <img src="${phone.image}" class="img-fluid" alt="phone-img">
+                    </div>
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                        <p class="card-text">Brand: ${phone.brand}</p>
+                    </div>
+                    <div class="py-2 d-flex justify-content-center">
+                        <button class="btn btn-secondary rounded-pill px-4">Show Details</button>
+                    </div>
                 </div>
-                <div class="card-body text-center">
-                    <h5 class="card-title">Name: ${phone.phone_name}</h5>
-                    <p class="card-text">Brand: ${phone.brand}</p>
-                </div>
-                <div class="py-2 d-flex justify-content-center">
-                    <button class="btn btn-secondary rounded-pill px-5">Show Details</button>
-                </div>
-            </div>
-        `;
-        colCard.classList.add('col');
-        phonesContainerField.appendChild(colCard);
-        console.log(phone)
-    });
+            `;
+            colCard.classList.add('col');
+            phonesContainerField.appendChild(colCard);
+            console.log(phone)
+        });
+    }
 }
